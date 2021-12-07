d82855d36a8e0a24bb3cb9fef5802434aafdf059
sushmoy roy <sushmoyr@gmail.com>
2021-12-07 23:14:39 +0600

    V2 structure added

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//Global module imports
+const express = require('express');
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cookie_parser = require('cookie-parser');
+const cors = require('cors');
+//Local Module's Imports
+
+//configs
+const app = express();
+dotenv.config();
+
+const corsOptions = {
+    origin: "https://localhost:8080"
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+
+//DB connection
+mongoose.connect(process.env.DB_URL)
+    .then(r => console.log('Database Connected'))
+    .catch(err => console.log(err));
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running at ", Date());
+})
